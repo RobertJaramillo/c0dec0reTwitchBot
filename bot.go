1e976263fdfd5b8bb3aa896c60492f31ef4c6926
Robert Jaramillo <robert.jaramillo0484@gmail.com>
2023-02-01 22:22:20 -0500

    Added functionality to print error check speak

--- bot.go
+++ bot.go
@@ -265,8 +265,12 @@ func (ccb *C0deC0reBot) Speak(msg string) error {
 	}
 
 	fmt.Println("I am HERE")
+
+	comp_msg := fmt.Sprintf("PRIVMSG #%s %s\r\n", ccb.ChannelName, msg)
+
 	// Message was not empty so write the message to the screen
-	_, err := ccb.conn.Write([]byte(fmt.Sprintf("PRIVMSG #%s %s\r\n", ccb.ChannelName, msg)))
+	_, err := ccb.conn.Write([]byte(comp_msg))
+
 	fmt.Println("Passed write")
 	if nil != err {
 		return err
